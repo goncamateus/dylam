e8a04fc272d69e5d04f9b668ec3ce85f69e297c7
goncamateus <mateus.gm@gmail.com>
2024-03-04 09:36:06 -0300

    Redo some wwork

--- envs/__init__.py
+++ envs/__init__.py
@@ -3,7 +3,7 @@ from gymnasium.envs.registration import register
 
 register(
     id="mo-LunarLanderContinuous-v2",
-    entry_point="envs.lunar_lander.lunar_lander:LunarLanderStrat",
+    entry_point="envs.lunar_lander:LunarLanderStrat",
     kwargs={"continuous": True},
     max_episode_steps=1000,
     reward_threshold=200,
@@ -11,20 +11,13 @@ register(
 
 register(
     id="mo-Hopper-v4",
-    entry_point="envs.hopper.hopper:Hopper",
+    entry_point="envs.hopper:Hopper",
     max_episode_steps=1000,
 )
 
 
 register(
     id="mo-Pendulum-v1",
-    entry_point="envs.pendulum.pendulum:Pendulum",
+    entry_point="envs.pendulum:Pendulum",
     max_episode_steps=200,
-)
-
-
-register(
-    id="mo-FlappyBird-v0",
-    entry_point="envs.flappy_bird.flappy_bird:FlappyBird",
-    max_episode_steps=1000,
 )
